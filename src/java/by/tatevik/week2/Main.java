f7f0c2ee95c8688c23af0f84143ebdaf05cc4e30
Tatevik777 <67191920+Tatevik777@users.noreply.github.com>
2024-10-17 15:11:43 +0300

    homework week2 done

---
+++ src/java/by/tatevik/week2/Main.java
@@ -0,0 +1,90 @@
+package by.tatevik.week2;
+
+public class Main {
+    public static void main(String[] args) {
+        //byte a = 55;
+        //System.out.println(a);
+        //int b = 555;
+        //System.out.println(b);
+        //float j=2222222;
+        // System.out.println(j);
+        //short c = 1001;
+        //System.out.println(c);
+        //long l=-555555555;
+        //System.out.println(l);
+        //double e= 7.777;
+        //System.out.println(e);
+        //boolean t =true;
+        //System.out.println(t);
+        // char f='а';
+        //System.out.println(f);
+        //String myString="Hakuna Matata";
+        //System.out.println(myString);
+        //char letter='ы';
+        //System.out.println(letter);
+        //char letter1=1067;
+        //System.out.println(letter1);
+        //char letter2= '\u042b';
+        // System.out.println(letter2);
+
+        // String myString2="Life is good";
+        // System.out.println(myString2);
+        // String myString3=new String("HakunaMatata");
+        //System.out.println(myString3);
+        // String myString4;
+        //myString4="Don't worry, be happy";
+        // System.out.println(myString4);
+        //byte a = 55;
+        //Byte box1;
+        //box1=new Byte(a);
+        // System.out.println(box1);
+
+        //int b = 555;
+        //Integer box2;
+        //box2=Integer.valueOf(b);
+        //System.out.println(box2);
+
+        // double e= 7.777;
+        //Double box3;
+        //box3=e;
+        //System.out.println(box3);
+
+        // Byte a2 = 55;
+        // byte unbox1;
+        // unbox1=a2.byteValue();
+        //System.out.println(unbox1);
+
+        //Integer b2 = 555;
+        // intint unbox2;
+        // unbox2=b2;
+        // System.out.println(unbox2);
+
+        // Double e2= 7.777;
+        // double unbox3;
+        //  unbox3=e2;
+        //System.out.println(unbox3);
+
+        // int triangleBase = 5;
+        //int triangleSide = 8;
+        //int sqr = (triangleSide * triangleBase) / 2;
+        //System.out.println("Площадь тругольника " + sqr);
+        // int perimetr = (triangleSide * 2) + triangleBase;
+        // System.out.println("Периметр тругольника " + perimetr);
+
+        //  int b = 50;
+        //  int c = 20;
+
+        //  System.out.println(++b);
+        //  System.out.println(--b);
+        //  System.out.println(++c);
+        //  System.out.println(--c);
+        //  System.out.println(b >= 51 && c < 22);
+
+        String javaString = "Training Java";
+        String currentDate = "13 октября 2024";
+        System.out.println(javaString.length());
+        System.out.println(javaString.charAt(5));
+        System.out.println(javaString + " " + currentDate);
+
+    }
+}
