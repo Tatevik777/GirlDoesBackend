ca6448145ac8ebd76037d4e2bb2cda8744f495a8
Tatevik777 <67191920+Tatevik777@users.noreply.github.com>
2024-10-17 16:00:57 +0300

    extra task calculator done

--- src/java/by/tatevik/simpleCalc/Main.java
+++ src/java/by/tatevik/simpleCalc/Main.java
@@ -5,11 +5,11 @@ import by.tatevik.simpleCalc.util.Calculator;
 
 public class Main {
     public static void main(String[] args) {
-        int result = Calculator.sum(5, 5);
+        int result1 = Calculator.sum(5, 5);
         int result2 = Calculator.multiply(55, 55);
         int result3 = Calculator.substract(1055, 55);
 
-        ResultWriterService.printResult(5, 5, "сложение", result);
+        ResultWriterService.printResult(5, 5, "сложение", result1);
         ResultWriterService.printResult(55, 55, "умножение", result2);
         ResultWriterService.printResult(1055, 55, "вычитание", result3);
     }
